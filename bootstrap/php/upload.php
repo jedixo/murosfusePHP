1d4300a5e8f712fd3e1dfb32c1b4a2d5a9e39ef3
jake Dixon <jake.dixon@my.jcu.edu.au>
2016-05-25 02:42:44 +1000

    uploads fully work under 2mb (uni network limitation)

--- bootstrap/php/upload.php
+++ bootstrap/php/upload.php
@@ -25,7 +25,7 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        header('location:../index.php');
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
